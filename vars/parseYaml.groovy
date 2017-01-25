327940b572f82856c0087ccce70a0dee0e1c9a25
Marat G <maratoid@gmail.com>
2017-01-25 11:46:00 -0800

    add yaml parsing lib

---
+++ vars/parseYaml.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/groovy
+import com.cloudbees.groovy.cps.NonCPS
+import org.yaml.snakeyaml.Yaml
+
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
+  return parseYaml(config.json)
+}
+
+@NonCPS
+def parseYaml(yamlText) {
+  def yaml = new Yaml()
+  return yaml.load(jsonText)
+}
