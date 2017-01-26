decf393bcc7fe58635eec52af27e72168b2ed153
Marat G <maratoid@gmail.com>
2017-01-26 13:03:49 -0800

    add functions to load pipeline settings from remote repos

---
+++ src/net/zonarsystems/pipeline/PipelineBuilders.groovy
@@ -0,0 +1,21 @@
+package net.zonarsystems.pipeline
+
+def loadPipelineConfig() {
+  return fileLoader.fromGit(
+    'pipeline', 
+    'https://github.com/samsung-cnct/zonar-pipeline.git', 
+    'master', 
+    'repo-scan-access', 
+    ''
+  )
+}
+
+def loadPipelineSettings() {
+  return fileLoader.fromGit(
+    'settings', 
+    'https://github.com/samsung-cnct/zonar-pipeline.git', 
+    'master', 
+    'repo-scan-access', 
+    ''
+  )
+}
