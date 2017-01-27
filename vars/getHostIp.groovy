ed70bec9c9d91a031834c611fa5e485607552665
Marat G <maratoid@gmail.com>
2017-01-27 11:13:02 -0800

    add a func to query dns for an ip address

---
+++ vars/getHostIp.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/groovy
+def call(hostname) {  
+  return getHostIp(hostname)
+}
+
+def getHostIp(hostname) {
+  return sh(returnStdout: true, script: "getent hosts ${hostname} | awk '{ print $1 }'").trim()
+}
