ca9edd7ac52004df4ca106f90272f5a2451ba7bc
Marat G <maratoid@gmail.com>
2017-01-27 16:10:51 -0800

    add get helm chart version

--- vars/getChartVersion.groovy
+++ vars/getChartVersion.groovy
@@ -4,5 +4,9 @@ def call(chartName) {
 }
 
 def getChartVersion(chartName) {
-  return sh(returnStdout: true, script: "helm inspect chart ${chartName} | grep version | awk '{ print \$2 }'").trim()
+  if (sh(returnStatus: true, script: "helm inspect chart ${chartName}") != 0) {
+    return null
+  } else {
+    return sh(returnStdout: true, script: "helm inspect chart ${chartName} | grep version | awk '{ print \$2 }'").trim()
+  }
 }
