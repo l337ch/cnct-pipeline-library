5ad51eb8fb8294affcbf8b0f083a0953c0170e28
Marat G <maratoid@gmail.com>
2017-01-30 00:20:00 -0800

    fix listFolders

--- vars/listFolders.groovy
+++ vars/listFolders.groovy
@@ -4,7 +4,7 @@ def call(path) {
   def dirNames = sh(returnStdout: true, script: "ls -dm -- ${path}/*/ | tr -d ' '").trim().split(',')
 
   for (i = 0; i < dirNames.size(); i++) {
-    dirs << "${dirNames[i]}"
+    dirs << "${dirNames[i][0..-2]}"
   }
   return dirs
 }
