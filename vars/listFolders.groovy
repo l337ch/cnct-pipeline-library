36280b377d6b0a2b6bc8de2fc63a6a2f76d263ea
Marat G <maratoid@gmail.com>
2017-01-31 10:46:24 -0800

    check if path actually exists when getting folder list

--- vars/listFolders.groovy
+++ vars/listFolders.groovy
@@ -1,10 +1,13 @@
 #!/usr/bin/groovy
 def call(path) {  
   def dirs = []
-  def dirNames = sh(returnStdout: true, script: "ls -dm -- ${path}/*/ | tr -d ' '").trim().split(',')
+  if (fileExists(path)) {
+    def dirNames = sh(returnStdout: true, script: "ls -dm -- ${path}/*/ | tr -d ' '").trim().split(',')
 
-  for (i = 0; i < dirNames.size(); i++) {
-    dirs << "${dirNames[i][0..-2]}"
+    for (i = 0; i < dirNames.size(); i++) {
+      dirs << "${dirNames[i][0..-2]}"
+    }
   }
+
   return dirs
 }
