11dc0cb0926caa829a7f38e5e9758d1cf05f3952
Marat G <maratoid@gmail.com>
2017-02-08 16:50:29 -0800

    in progress

--- src/net/zonarsystems/pipeline/Pipeline.groovy
+++ src/net/zonarsystems/pipeline/Pipeline.groovy
@@ -1,32 +1,59 @@
 package net.zonarsystems.pipeline
 
-trait Pipeline implements Serializable {                           
+trait Pipeline {                           
   def steps
   def application
-  def env
 
-  private settings = fileLoader.fromGit(
-    'settings', 
-    'https://github.com/samsung-cnct/zonar-pipeline.git', 
-    'master', 
-    'repo-scan-access', 
-    ''
-  ).getConfig()
-  private pipeline = fileLoader.fromGit(
-    'pipeline', 
-    "https://github.com/samsung-cnct/zonar-pipeline.git", 
-    'master', 
-    "repo-scan-access", 
-    ''
-  ).getConfig() 
+  private ready = false
+  private def setReady() { ready = true }
+  private def bailOnUninitialized() { if (!ready) { throw new Exception('Pipeline not initialized') } }
+  def init() {
+    steps.podTemplate(label: "env-${getApplication()}", containers: [], volumes: []) {
+      steps.node ("env-${getApplication()}") {
+        settings = fileLoader.fromGit(
+          'settings', 
+          'https://github.com/samsung-cnct/zonar-pipeline.git', 
+          'master', 
+          'repo-scan-access', 
+          ''
+        ).getConfig()
+        setSettings(settings)
 
-  private helpers = new PipelineHelpers(
-    this.steps, 
-    this.settings, 
-    this.pipeline
-  )
+        pipeline = fileLoader.fromGit(
+          'pipeline', 
+          "https://github.com/samsung-cnct/zonar-pipeline.git", 
+          'master', 
+          "repo-scan-access", 
+          ''
+        ).getConfig()
+        setPipeline(pipeline)
 
-  def getSettings() { settings }
-  def getPipeline() { pipeline }
-  def getHelpers() { helpers }
+        setEnv(env)
+        setHelpers()
+      }
+    }
+  }
+
+  private env
+  def getEnv() { bailOnUninitialized(); env }
+  private def setEnv(env) { this.env = env }
+
+  private settings
+  def getSettings() { bailOnUninitialized(); settings }
+  private def setSettings(settings) { this.settings = settings }
+
+  private pipeline
+  def getPipeline() { bailOnUninitialized(); pipeline[application] }
+  private def setPipeline(pipeline) { this.pipeline = pipeline }
+  
+  private helpers
+  def getHelpers() { bailOnUninitialized(); helpers }
+  private def setHelpers() { 
+    if (!settings || !pipeline) {
+      throw new Exception('Set settings and pipeline info first.')
+    } 
+    helpers = new PipelineHelpers(steps, settings, pipeline) 
+  }
+
+  abstract def pipelineRun()  
 }
