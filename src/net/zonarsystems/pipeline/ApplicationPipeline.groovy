d27aa2745ffb49d9a3d0843749d5c557603e91da
Marat G <maratoid@gmail.com>
2017-02-09 13:40:55 -0800

    start trying to track down unsupported operation exception

--- src/net/zonarsystems/pipeline/ApplicationPipeline.groovy
+++ src/net/zonarsystems/pipeline/ApplicationPipeline.groovy
@@ -4,14 +4,13 @@ import hudson.EnvVars
 import org.jenkinsci.plugins.workflow.cps.EnvActionImpl
 import hudson.model.Cause
 
-class ApplicationPipeline implements Pipeline, Serializable {
+class ApplicationPipeline implements Serializable {
   def steps
   def application
 
   ApplicationPipeline(steps, application) {
     this.steps = steps
     this.application = application
-    init();
   }
 
   def pipelineRun() {
