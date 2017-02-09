d6f0afe2019a3a53b4e3f7be7558dea4218d8699
Marat G <maratoid@gmail.com>
2017-02-09 13:32:12 -0800

    start trying to track down unsupported operation exception

--- src/net/zonarsystems/pipeline/Pipeline.groovy
+++ src/net/zonarsystems/pipeline/Pipeline.groovy
@@ -4,48 +4,9 @@ trait Pipeline {
   def steps
   def application
 
-  private ready = false
-  private def bailOnUninitialized() { if (!ready) { throw new Exception('Pipeline not initialized, run init() first') } }
-
-  private env
-  def getEnv() { bailOnUninitialized(); env }
-
-  private settings
-  def getSettings() { bailOnUninitialized(); settings }
-  
-  private pipeline
-  def getPipeline() { bailOnUninitialized(); pipeline[application] }
-  
-  private helpers
-  def getHelpers() { bailOnUninitialized(); helpers }
-
   // init things that need node context
   def init() {
-    steps.podTemplate(label: "env-${getApplication()}", containers: [], volumes: []) {
-      steps.node ("env-${getApplication()}") {
-        this.settings = fileLoader.fromGit(
-          'settings', 
-          'https://github.com/samsung-cnct/zonar-pipeline.git', 
-          'master', 
-          'repo-scan-access', 
-          ''
-        ).getConfig()
-
-        this.pipeline = fileLoader.fromGit(
-          'pipeline', 
-          "https://github.com/samsung-cnct/zonar-pipeline.git", 
-          'master', 
-          "repo-scan-access", 
-          ''
-        ).getConfig()
-
-        this.env = env
-        this.helpers = new PipelineHelpers(steps, settings, pipeline)
-      }
-    }
-
-    // set ready
-    ready = true
+    
   }
 
   abstract def pipelineRun()  
