b4d1c3bf4a000cf0ad37f77a63c2993dacd21e1a
Marat G <maratoid@gmail.com>
2017-03-15 11:03:19 -0700

    config -> e2e

--- vars/getE2eVars.groovy
+++ vars/getE2eVars.groovy
@@ -5,7 +5,7 @@ def call(body) {
   // evaluate the body block, and collect configuration into the object
   def config = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = e2e
+  body.delegate = config
   body()
   
   return getE2eVars(config.e2e)
