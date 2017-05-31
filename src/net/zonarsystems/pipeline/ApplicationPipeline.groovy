3b565a0ef92bb25711c3ba17250c452351af9f3a
john.felten@gmail.com <john@zenzic.com>
2017-05-31 09:37:52 -0700

    remove shell executable to make it work

--- src/net/zonarsystems/pipeline/ApplicationPipeline.groovy
+++ src/net/zonarsystems/pipeline/ApplicationPipeline.groovy
@@ -183,7 +183,7 @@ class ApplicationPipeline implements Serializable {
     getSteps().echo ("Checking for newer version of ${packageName} in image ${dockerImage}")
     //use yum to check the installed and available version
     getSteps().echo "gcloud docker -- run -it ${dockerImage} yum list installed ${packageName} | awk \'END {print \$2 }\'"
-    getSteps().sh "ls"
+    
     //def availableVersion = getSteps().sh(script: "gcloud docker -- run -it ${dockerImage} yum list available ${packageName} | awk \'END {print \$2 }\'", returnStdout: true)
     //getSteps().echo "${packageName} is at ${currentVersion} latest=${availableVersion}"
     //if (currentVersion != availableVersion) {
@@ -195,7 +195,7 @@ class ApplicationPipeline implements Serializable {
   }
 
   def isNewZonarReleaseAvailable() {
-    //getSteps().stage('Checking if Zonar has released new artifacts for this chart'){
+    getSteps().stage('Checking if Zonar has released new artifacts for this chart'){
       def chartsFolders=getScript().listFolders('./charts')
       for(def i=0;i<chartsFolders.size();i++){
         getSteps().echo "checking for new packages in chart: ${chartsFolders[i]}"
@@ -220,7 +220,7 @@ class ApplicationPipeline implements Serializable {
           }
         }
       }
-    //}
+    }
   }
 
   /**
